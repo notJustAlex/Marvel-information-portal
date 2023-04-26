381f3768b0f206aa209ee47871777bcd4eb95272
Alex <alex.zakablook@gmail.com>
2023-04-26 18:33:20 +0200

    second commit

---
+++ src/components/errorBoundary/ErrorBoundery.js
@@ -0,0 +1,24 @@
+import { Component } from "react";
+import ErrorMessage from "../errorMessage/ErrorMessage";
+
+class ErrorBoundery extends Component {
+    state = {
+        error: false
+    }
+
+    componentDidCatch(error, errorInfo) {
+        this.setState({
+            error: true
+        })
+    }
+
+    render() {
+        if(this.state.error) {
+            return <ErrorMessage/>
+        }
+
+        return this.props.children;
+    }
+}
+
+export default ErrorBoundery;
