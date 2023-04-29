02514eedb85e14e1e1bed08fce7c2cd9b3e5ea98
Alex <alex.zakablook@gmail.com>
2023-04-29 16:10:46 +0200

    animations

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -1,5 +1,6 @@
 import { useState, useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
+import { Transition, CSSTransition, TransitionGroup } from 'react-transition-group';
 
 import Spinner from '../spinner/Spinner';
 import ErrorMessage from '../errorMessage/ErrorMessage';
@@ -54,7 +55,8 @@ const CharList = (props) => {
             }
 
             return (
-                <li 
+                <CSSTransition key={item.id} timeout={500} classNames="char__item">
+                    <li 
                     className="char__item"
                     tabIndex={0}
                     ref={elem => itemRefs.current[i] = elem}
@@ -68,16 +70,19 @@ const CharList = (props) => {
                             props.onCharSelected(item.id);
                             focusOnItem(i);
                         }
-                }}>
-                    <img src={item.thumbnail} alt={item.name} style={imgStyle}/>
-                    <div className="char__name">{item.name}</div>
-                </li>
+                    }}>
+                        <img src={item.thumbnail} alt={item.name} style={imgStyle}/>
+                        <div className="char__name">{item.name}</div>
+                    </li>
+                </CSSTransition>
             )
         });
 
         return (
             <ul className="char__grid">
-                {items}
+                <TransitionGroup component={null}>
+                    {items}
+                </TransitionGroup>
             </ul>
         )
     }
